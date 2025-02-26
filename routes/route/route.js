69d08a1d45d260828816ffdafb8727b402204769
louis <15221936+louis014@user.noreply.gitee.com>
2025-02-26 19:14:22 +0800

    feat: 路径规划功能实现

--- routes/route/route.js
+++ routes/route/route.js
@@ -8,14 +8,44 @@ const baseUrl = "https://restapi.amap.com/v3/direction";
 
 router.post("/getRoute", async (req, res) => {
   const { origin, destination, wayCode } = req.body;
-  console.log(baseUrl + way[wayCode], "@@@@@@@@@");
+  console.log(origin, destination, wayCode, "@@@@@@@");
+
+  // 封装获取经纬度的函数
+  const getLocation = async (address) => {
+    try {
+      const response = await axios.post(
+        "https://restapi.amap.com/v3/geocode/geo",
+        null, // 第二个参数是请求体，所以传 null
+        {
+          params: {
+            key,
+            address
+          }
+        }
+      );
+      const geocodes = response.data.geocodes;
+      if (geocodes && geocodes.length > 0) {
+        return geocodes[0].location;
+      } else {
+        throw new Error("未找到地址的经纬度信息");
+      }
+    } catch (error) {
+      console.error("获取经纬度信息出错:", error);
+      throw error;
+    }
+  };
 
   try {
+    // 获取起点和终点的经纬度
+    const originLocation = await getLocation(origin);
+    const destinationLocation = await getLocation(destination);
+
+    // 请求路线信息
     const response = await axios.get(baseUrl + way[wayCode], {
       params: {
         key,
-        origin,
-        destination
+        origin: originLocation,
+        destination: destinationLocation
       }
     });
     console.log(response.data);
